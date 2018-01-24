ad31bf2e15febde47682a69d46bb37aa53f3f273
ShannonChenCHN <273879688@qq.com>
2018-01-24 19:30:14 +0800

    Update tutorials for NSURLSession

--- iOSTutorials/网络/NSURLSession/NSURLSessionExample/NSURLSessionExample/ViewController.m
+++ iOSTutorials/网络/NSURLSession/NSURLSessionExample/NSURLSessionExample/ViewController.m
@@ -7,7 +7,11 @@
 //
 
 #import "ViewController.h"
-#import "Test.h"
+
+
+#define SCFileBoundary @"MalJob"
+#define SCNewLine @"\r\n"
+#define SCEncode(str) [str dataUsingEncoding:NSUTF8StringEncoding]
 
 @interface ViewController ()
 
@@ -29,15 +33,15 @@
     // 1.创建url
     NSURL *url = [NSURL URLWithString:@"https://www.baidu.com"];
     
-    // 2.创建请求 并：设置缓存策略为每次都从网络加载 超时时间30秒
+    // 2.创建请求 并设置缓存策略为每次都从网络加载，超时时间30秒
     NSURLRequest *request = [NSURLRequest requestWithURL:url
                                              cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
                                          timeoutInterval:30];
     
-    // 3.采用苹果提供的共享session
+    // 3.创建 task
+//    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:nil];
     NSURLSession *sharedSession = [NSURLSession sharedSession];
-    
-    // 4.由系统直接返回一个dataTask任务
     NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         // 网络请求完成之后就会执行，NSURLSession自动实现多线程
         NSLog(@"%@",[NSThread currentThread]);
@@ -92,22 +96,150 @@
     [downloadTask resume];
 }
 
+/// 通过提交 form 表单的形式上传文件
+/// iOS 表单上传文件 https://www.jianshu.com/p/2c3644202a98
+/// 四种常见的 POST 提交数据方式对应的content-type取值 http://www.cnblogs.com/wushifeng/p/6707248.html
+/// iOS之网络请求之AFN表单上传之form-data http://blog.csdn.net/kws959844005/article/details/52487358
+- (void)uploadingFormDataUsingAnNSDataObject {
+    
+    // 1.创建 url，参数
+    NSURL *url = [NSURL URLWithString:@"https://www.example.com"];
+    NSDictionary *params = @{
+                             @"name" : @"xxxx",
+                             @"password" : @"xxxxx",
+                             };
+    
+    // 2.创建一个POST请求
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    
+    // 3. 获取文件信息
+    NSURL *filePath = [[NSBundle mainBundle] URLForResource:@"filename" withExtension:@"txt"];
+    NSData *fileData = [NSData dataWithContentsOfURL:filePath];
+    NSString *filename = @"file.txt";
+    NSString *mimeType = [self getMIMETypeOfFileWithPath:filePath];
+    
+    // 4.设置请求体
+    NSMutableData *body = [NSMutableData data];
+    
+    // 4.1.文件参数
+    [body appendData:SCEncode(@"--")];
+    [body appendData:SCEncode(SCFileBoundary)];
+    [body appendData:SCEncode(SCNewLine)];
+    
+    NSString *disposition = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"", filename];
+    [body appendData:SCEncode(disposition)];
+    [body appendData:SCEncode(SCNewLine)];
+    
+    NSString *type = [NSString stringWithFormat:@"Content-Type: %@", mimeType];
+    [body appendData:SCEncode(type)];
+    [body appendData:SCEncode(SCNewLine)];
+    
+    [body appendData:SCEncode(SCNewLine)];
+    [body appendData:fileData];
+    [body appendData:SCEncode(SCNewLine)];
+    
+    // 4.2.非文件参数
+    [params enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [body appendData:SCEncode(@"--")];
+        [body appendData:SCEncode(SCFileBoundary)];
+        [body appendData:SCEncode(SCNewLine)];
+        
+        NSString *disposition = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"", key];
+        [body appendData:SCEncode(disposition)];
+        [body appendData:SCEncode(SCNewLine)];
+        
+        [body appendData:SCEncode(SCNewLine)];
+        [body appendData:SCEncode([obj description])];
+        [body appendData:SCEncode(SCNewLine)];
+    }];
+    
+    // 4.3.结束标记
+    [body appendData:SCEncode(@"--")];
+    [body appendData:SCEncode(SCFileBoundary)];
+    [body appendData:SCEncode(@"--")];
+    [body appendData:SCEncode(SCNewLine)];
+    
+    request.HTTPBody = body;
+    
+    // 5.设置请求头(告诉服务器这次传给你的是form表单文件数据，告诉服务器现在发送的是一个文件上传请求)
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", SCFileBoundary];
+    [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    // 6. 发起请求
+    NSURLSession *sharedSession = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    
+    [dataTask resume];
+}
 
+/// 上传内存中的 NSData 对象
 - (void)uploadingBodyContentUsingAnNSDataObject {
     
-    // 创建要上传的 NSData
+    // 1. 创建要上传的 NSData
     NSURL *textFileURL = [NSURL fileURLWithPath:@"/path/to/file.txt"];
     NSData *data = [NSData dataWithContentsOfURL:textFileURL];
+    
+    // 2. 创建请求
+    NSURL *url = [NSURL URLWithString:@"https://www.example.com/"];
+    NSMutableURLRequest *mutableRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    // 2.1 设置请求方式
+    mutableRequest.HTTPMethod = @"POST";
+    
+    // 2.2 设置请求头（告诉服务器这次传给你的是 text 文件数据）
+    [mutableRequest setValue:[NSString stringWithFormat:@"%@", @(data.length)] forHTTPHeaderField:@"Content-Length"];
+    [mutableRequest setValue:@"text/plain" forHTTPHeaderField:@"Content-Type"];
+    
+    // 3.创建 session，采用苹果提供全局的共享session
+    NSURLSession *sharedSession = [NSURLSession sharedSession];
+    
+    // 4. 创建任务
+    NSURLSessionUploadTask *uploadTask = [sharedSession uploadTaskWithRequest:mutableRequest
+                                                                     fromData:data
+                                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+                                                                
+    }];
+    [uploadTask resume];
+}
+
+/// 上传本地文件
+- (void)uploadingBodyContentUsingAFile {
+    // 获取磁盘文件地址
+    NSURL *textFileURL = [NSURL fileURLWithPath:@"/path/to/file.txt"];
     
     // 创建请求
     NSURL *url = [NSURL URLWithString:@"https://www.example.com/"];
     NSMutableURLRequest *mutableRequest = [NSMutableURLRequest requestWithURL:url];
     mutableRequest.HTTPMethod = @"POST";
-    [mutableRequest setValue:[NSString stringWithFormat:@"%@", @(data.length)] forHTTPHeaderField:@"Content-Length"];
-    [mutableRequest setValue:@"text/plain" forHTTPHeaderField:@"Content-Type"];
     
-    NSURLSessionUploadTask *uploadTask = [defaultSession uploadTaskWithRequest:mutableRequest fromData:data];
+    // 创建 session，采用苹果提供全局的共享session
+    NSURLSession *sharedSession = [NSURLSession sharedSession];
+    
+    NSURLSessionUploadTask *uploadTask = [sharedSession uploadTaskWithRequest:mutableRequest
+                                                                     fromFile:textFileURL
+                                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
     [uploadTask resume];
+}
+
+#pragma mark - Helper
+
+- (NSString *)getMIMETypeOfFileWithPath:(NSURL *)url {
+    
+    // 1.创建一个请求
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // 2.发送请求（返回响应）
+    NSURLResponse *response = nil;
+    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
+    
+    // 3.获得MIMEType
+    return response.MIMEType;
 }
 
 
--- iOSTutorials/网络/NSURLSession/README.md
+++ iOSTutorials/网络/NSURLSession/README.md
@@ -12,31 +12,85 @@ NSURLConnection 作为 Core Foundation / CFNetwork 框架的 API 之上的一个
 
 和 NSURLConnection 一样，NSURLSession 指的也不仅是同名类 NSURLSession，还包括一系列相互关联的类。NSURLSession 包括了与之前相同的组件，NSURLRequest 与 NSURLCache，但是把 NSURLConnection 替换成了 NSURLSession、NSURLSessionConfiguration 以及 NSURLSessionTask 的 3 个子类：NSURLSessionDataTask，NSURLSessionUploadTask，NSURLSessionDownloadTask。                                                                                       
 
-![](https://koenig-media.raywenderlich.com/uploads/2017/06/url_session_diagram_1.png)
+![](../images/url_session_diagram_1.png)
 
 
-### 二、NSURLSession 的使用
+### 二、URL Loading System
+
+URL Loading System 是一系列的用来访问通过 URL 来定位的资源的类和协议。这项技术的核心在于基于 `NSURL` 来访问资源，除了加载 URL 的类之外，我们把其他相关辅助类分为 5 类：
+
+-  协议支持（protocol support）
+-  权限认证（authentication and credentials）
+-  cookie 存储（cookie storage）
+-  请求配置（configuration management）
+-  缓存管理（cache management）
+
+
+![](../images/nsobject_hierarchy_2x.png)
+
+#### 1. URL 加载
+
+URL Loading System 最常用的类就是用来根据 URL 请求数据的类 `NSURLSession`。按照获取数据后保存的位置，可以分为两种形式。
+
+- 获取二进制数据（内存）
+- 下载文件到本地
+
+#### 2. 辅助类
+
+2.1 URL 请求
+
+`NSURLRequest` 将 URL 和请求协议相关的属性封装起来了。
+比如，支持 HTTP 协议的 `NSURLRequest`/`NSMutableURLRequest ` 类的就包括读取/设置请求方式、请求体、请求头等属性的方法。
+
+2.2 响应元数据
+
+服务器返回的数据包括两部分：
+
+- 描述内容数据的元数据
+- 内容数据本身
+
+描述内容数据的元数据往往是请求协议定义的，`NSURLResponse ` 会将元数据和内容数据本身封装起来，大部分协议都有的元数据包括 MIME type、 expected content length、text encoding (where applicable)、以及产生这个响应的 URL。
+
+
+#### 3. 重定向（改变请求）
+
+有些协议，比如 HTTP，提供了一种重定向机制：当你发起一个请求时，而你请求的资源的 URL 已经发生改变了，服务器就会告诉客户端你请求的资源已经被转移到了新的 URL。
+
+我们可以通过实现相关的代理方法，来拦截重定向事件，并决定是否需要重定向到新的地址。
+
+#### 4. 权限认证
+有些服务器会对某些特定的内容限制访问权限，只对提供了信任证书通过认证的用户提供访问资格。
+
+URL Loading System 提供了封装证书（credentials ）和保存安全凭证（secure credential）的类：
+
+- `NSURLCredential`：
+- `NSURLCredentialStorage`
+- `NSURLAuthenticationChallenge`
+
+
+#### 5. 缓存管理
+
+#### 6. cookie 存储
+
+#### 7. 协议支持
+
+
+### 三、NSURLSession 的使用
 NSURLSession 负责发送请求和接收响应，
 
 #### 1. NSURLSessionTask
 
-![](https://www.objccn.io/images/issues/issue-5/NSURLSession.png)
+![](../images/NSURLSession.png)
 
 
 #### 2. NSURLSession 的 delegate 方法
 
 #### 3. NSURLSessionConfiguration
 
-### 三、NSURLSession 和 NSURLConnection 的对比
+### 四、NSURLSession 和 NSURLConnection 的对比，为什么 NSURLConnection 会被 NSURLSession 所替代？
 
 问题：
-1. 什么是 URL Loading System？
 
-2. NSURLConnection 与 NSURLSession 的区别？
-
-3. 为什么 NSURLConnection 会被 NSURLSession 所替代？
-
-4. 如何使用 NSURLConnection 和 NSURLSession
 
 5. 何时需要使用以及如何使用 NSURLProtocol？
 
